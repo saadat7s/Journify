d733a1acba5b525ed4839a6464e0b9ecb231c4b5
sickbeoi <saadatullah21@gmail.com>
2023-12-16 18:41:46 +0500

    Added toastifier for user feedback

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// Import necessary components and styles
 import Navbar from './components/Navbar';
 import Body from './components/Body';
 import EditEntry from './components/EditEntry';
 import AddEntry from './components/AddEntry';
 import LoginPage from './components/LoginPage';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
-    <>
     <Router>
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Body />} />
-        <Route path="/edit/:id" element={<EditEntry />} />
-        <Route path="/Add" element={<AddEntry />} />
-        <Route path="/Login" element={<LoginPage/>}/>
-        </Routes>
-      <ToastContainer />
-    </div>
-  </Router></>
-    
+      <div className="App">
+        <Navbar />
+        <div className="main-container">
+          <div className="content-wrapper">
+            <Routes>
+              <Route path="/" element={<Body />} />
+              <Route path="/edit/:id" element={<EditEntry />} />
+              <Route path="/add" element={<AddEntry />} />
+              <Route path="/login" element={<LoginPage />} />
+            </Routes>
+            <Footer />
+            <ToastContainer/>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
